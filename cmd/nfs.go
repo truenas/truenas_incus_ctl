24b10b0f6a4f9a736518ee92300d8fb92ee0c707
Jack Bendtsen <jack.bendtsen@mactrix.com>
2025-01-31 17:42:16 +1100

    Added options to nfs create,update, started implementing nfs list

--- cmd/nfs.go
+++ cmd/nfs.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"log"
+	"strconv"
 	"strings"
 	"truenas/admin-tool/core"
 
@@ -61,11 +62,11 @@ var nfsInspectCmd = &cobra.Command{
 
 func init() {
 	nfsCreateCmd.Run = func(cmd *cobra.Command, args []string) {
-		createOrUpdateNfs(cmd, ValidateAndLogin(), args)
+		createNfs(ValidateAndLogin(), args)
 	}
 
 	nfsUpdateCmd.Run = func(cmd *cobra.Command, args []string) {
-		createOrUpdateNfs(cmd, ValidateAndLogin(), args)
+		updateNfs(ValidateAndLogin(), args)
 	}
 
 	nfsDeleteCmd.Run = func(cmd *cobra.Command, args []string) {
@@ -80,6 +81,29 @@ func init() {
 		inspectNfs(ValidateAndLogin(), args)
 	}
 
+	createUpdateCmds := []*cobra.Command{nfsCreateCmd, nfsUpdateCmd}
+	for _, cmd := range createUpdateCmds {
+		cmd.Flags().Bool("read-only", false, "Export as write protected, default false")
+		cmd.Flags().Bool("ro", false, "Equivalent to --read-only=true")
+		cmd.Flags().String("comment", "", "")
+		cmd.Flags().String("networks", "", "A list of authorized networks that are allowed to access the share " +
+			"using CIDR notation. If empty, all networks are allowed")
+		cmd.Flags().String("hosts", "", "List of hosts")
+		cmd.Flags().String("maproot-user", "", "")
+		cmd.Flags().String("maproot-group", "", "")
+		cmd.Flags().String("mapall-user", "", "")
+		cmd.Flags().String("mapall-group", "", "")
+		cmd.Flags().String("security", "", "Array of Enum(sys,krb5,krb5i,krb5p)")
+		cmd.Flags().Bool("enabled", false, "")
+	}
+
+	listInspectCmds := []*cobra.Command{nfsListCmd, nfsInspectCmd}
+	for _, cmd := range listInspectCmds {
+		cmd.Flags().String("name", "", "")
+		cmd.Flags().Int("id", -1, "")
+		cmd.Flags().String("query-filter", "", "")
+	}
+
 	nfsCmd.AddCommand(nfsCreateCmd)
 	nfsCmd.AddCommand(nfsUpdateCmd)
 	nfsCmd.AddCommand(nfsDeleteCmd)
@@ -89,7 +113,7 @@ func init() {
 	shareCmd.AddCommand(nfsCmd)
 }
 
-func createOrUpdateNfs(cmd *cobra.Command, api core.Session, args []string) {
+func createNfs(api core.Session, args []string) {
 	if api == nil {
 		return
 	}
@@ -101,12 +125,74 @@ func createOrUpdateNfs(cmd *cobra.Command, api core.Session, args []string) {
 	var builder strings.Builder
 	builder.WriteString("[{\"path\":")
 	core.WriteEncloseAndEscape(&builder, sharePath, "\"")
+
+	optionsUsed, _, allTypes := getCobraFlags(nfsCreateCmd)
+	nProps := 0
+	for propName, valueStr := range optionsUsed {
+		if nProps > 0 {
+			builder.WriteString(",")
+		}
+		core.WriteEncloseAndEscape(&builder, propName, "\"")
+		builder.WriteString(":")
+		if t, exists := allTypes[propName]; exists && t == "string" {
+			core.WriteEncloseAndEscape(&builder, valueStr, "\"")
+		} else {
+			builder.WriteString(valueStr)
+		}
+		nProps++
+	}
+
 	builder.WriteString("}]")
 
-	query := builder.String()
-	fmt.Println(query)
+	stmt := builder.String()
+	fmt.Println(stmt)
 
-	out, err := api.CallString("sharing.nfs.create", "10s", query)
+	out, err := api.CallString("sharing.nfs.create", "10s", stmt)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(out))
+}
+
+func updateNfs(api core.Session, args []string) {
+	if api == nil {
+		return
+	}
+	defer api.Close()
+
+	idStr := args[0]
+	_, err := strconv.Atoi(idStr)
+	if err != nil {
+		fmt.Errorf("ID \"%s\" was not a number", idStr)
+	}
+
+	var builder strings.Builder
+	builder.WriteString("[")
+	builder.WriteString(idStr)
+	builder.WriteString(",{")
+
+	optionsUsed, _, allTypes := getCobraFlags(nfsUpdateCmd)
+	nProps := 0
+	for propName, valueStr := range optionsUsed {
+		if nProps > 0 {
+			builder.WriteString(",")
+		}
+		core.WriteEncloseAndEscape(&builder, propName, "\"")
+		builder.WriteString(":")
+		if t, exists := allTypes[propName]; exists && t == "string" {
+			core.WriteEncloseAndEscape(&builder, valueStr, "\"")
+		} else {
+			builder.WriteString(valueStr)
+		}
+		nProps++
+	}
+
+	builder.WriteString("}]")
+
+	stmt := builder.String()
+	fmt.Println(stmt)
+
+	out, err := api.CallString("sharing.nfs.update", "10s", stmt)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -125,6 +211,34 @@ func listNfs(api core.Session, args []string) {
 		return
 	}
 	defer api.Close()
+
+	_, allOptions, _ := getCobraFlags(nfsListCmd)
+
+	var builder strings.Builder
+
+	builder.WriteString("[")
+
+	if name := allOptions["name"]; name != "" {
+		path := "/mnt/" + name
+		builder.WriteString("[[\"path\",\"=\",")
+		core.WriteEncloseAndEscape(&builder, path, "\"")
+		builder.WriteString("]]")
+	} else if id, _ := strconv.Atoi(allOptions["id"]); id >= 0 {
+		builder.WriteString("[[\"id\",\"=\",")
+		builder.WriteString(fmt.Sprint(id))
+		builder.WriteString("]]")
+	}
+
+	builder.WriteString("]")
+
+	stmt := builder.String()
+	fmt.Println(stmt)
+
+	out, err := api.CallString("sharing.nfs.query", "10s", stmt)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(out))
 }
 
 func inspectNfs(api core.Session, args []string) {
@@ -132,4 +246,6 @@ func inspectNfs(api core.Session, args []string) {
 		return
 	}
 	defer api.Close()
+
+	//_, allOptions, allTypes := getCobraFlags(nfsInspectCmd)
 }
