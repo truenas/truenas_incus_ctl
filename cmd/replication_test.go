da630e9c4fcbacfbdf51897191550d29442fa127
Jack Bendtsen <jackdbendtsen@gmail.com>
2025-03-03 15:51:16 +1100

    Updated replication test (thanks CI)

--- cmd/replication_test.go
+++ cmd/replication_test.go
@@ -9,9 +9,9 @@ func TestReplicationStart(t *testing.T) {
 		t,
 		replStartCmd,
 		startReplication,
-		map[string]interface{}{"options":"a=true,bar=4,custom=something","direction":"push","retention_policy":"none"},
+		map[string]interface{}{"options":"a=true,bar=4,custom=something","direction":"push","retention_policy":"none","name_regex":".*"},
 		[]string{"dozer/testing/test1","dozer/testing/test2"},
-		"[{\"a\":true,\"bar\":4,\"custom\":\"something\",\"direction\":\"PUSH\",\"recursive\":false,\"retention_policy\":\"NONE\","+
+		"[{\"a\":true,\"bar\":4,\"custom\":\"something\",\"direction\":\"PUSH\",\"name_regex\":\".*\",\"recursive\":false,\"retention_policy\":\"NONE\","+
 			"\"source_datasets\":[\"dozer/testing/test1\"],\"target_dataset\":\"dozer/testing/test2\",\"transport\":\"LOCAL\"}]",
 	))
 }
