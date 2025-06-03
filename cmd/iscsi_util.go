b989e2a103443ef23cc4b3e02e96924c58a2b0c4
Jack Bendtsen <jackdbendtsen@gmail.com>
2025-06-03 23:48:33 +1000

    Improve RunIscsiAdminTool error reporting

--- cmd/iscsi_util.go
+++ cmd/iscsi_util.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -451,11 +452,20 @@ func MaybeLaunchIscsiDaemon() error {
 }
 
 func RunIscsiAdminTool(args []string) (string, error) {
+	retriesLeft := 10
 begin:
 	out, err := core.RunCommand("iscsiadm", args...)
 	if err != nil && strings.HasPrefix(err.Error(), "iscsiadm: Could not scan /sys/class/iscsi_transport") {
 		time.Sleep(time.Duration(500) * time.Millisecond)
-		goto begin
+		retriesLeft--
+		if retriesLeft > 0 {
+			goto begin
+		}
+	}
+	if err != nil {
+		err = errors.New(err.Error() +
+			"\nMake sure you've started the iscsi service, eg."+
+			"\n./truenas_incus_ctl service start --enable iscsitarget")
 	}
 	return out, err
 }
